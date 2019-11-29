aa692a9ce5cae6d4818892229d59e81b8266b664
SPITZ CHRISTOPHER <christopher.spitz@etu.unistra.fr>
2019-11-29 15:30:17 +0100

    Calculatrice de base avec interface

--- Calculatrice_Et_Interface/Fenetre.java
+++ Calculatrice_Et_Interface/Fenetre.java
@@ -5,13 +5,14 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.BorderFactory;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.Dimension;
 import java.awt.Color;
 
 
-public class Fenetre extends JFrame {
+public class Fenetre extends JFrame  {
 	String[] tab_strButtons = {"1","2","3","4","5","6","7","8","9","0",".","=","C","+","-","*","/"};
 	JButton[] tabBoutons = new JButton[tab_strButtons.length];
 	private JPanel Calto = new JPanel();
@@ -29,18 +30,16 @@ public class Fenetre extends JFrame {
 		this.setSize(480,480);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 		creationElements();
-		
 		this.setContentPane(Calto);
-
 		this.setVisible(true);
 	}
 
 	private void creationElements(){
-		Ecran = new JLabel("");
+		Font police = new Font("Arial", Font.BOLD,20);
+		Ecran.setFont(police);
 		Ecran.setHorizontalAlignment(JLabel.RIGHT);
-		Ecran.setPreferredSize(new Dimension(500,40));
+		Ecran.setPreferredSize(new Dimension(480,40));
 		JPanel op = new JPanel();
 		op.setPreferredSize(new Dimension(110,480));
 		JPanel chiffre = new JPanel();
@@ -54,29 +53,36 @@ public class Fenetre extends JFrame {
 
 			switch(i){
 				case 11 : 
+					tabBoutons[i].addActionListener(new EgalListener());
 					chiffre.add(tabBoutons[i]);
 					break;
 				case 12 :
 					tabBoutons[i].setPreferredSize(dim2);
+					tabBoutons[i].addActionListener(new ResetListener());
 					op.add(tabBoutons[i]);
 					break;
 				case 13 :
-					tabBoutons[i].setPreferredSize(dim2); 
+					tabBoutons[i].setPreferredSize(dim2);
+					tabBoutons[i].addActionListener(new PlusListener()); 
 					op.add(tabBoutons[i]);
 					break;
 				case 14 : 
 					tabBoutons[i].setPreferredSize(dim2);
+					tabBoutons[i].addActionListener(new MoinsListener());
 					op.add(tabBoutons[i]);
 					break;
 				case 15 : 
 					tabBoutons[i].setPreferredSize(dim2);
+					tabBoutons[i].addActionListener(new MulListener());
 					op.add(tabBoutons[i]);
 					break;
 				case 16 : 
 					tabBoutons[i].setPreferredSize(dim2);
+					tabBoutons[i].addActionListener(new DivListener());
 					op.add(tabBoutons[i]);
 					break;
 				default :
+					tabBoutons[i].addActionListener(new ChiffreListener());
 					chiffre.add(tabBoutons[i]);
 					break;
 			}
@@ -87,6 +93,59 @@ public class Fenetre extends JFrame {
 	Calto.add(chiffre,BorderLayout.CENTER);
 	Calto.add(op,BorderLayout.EAST);
 	}
+
+	class ChiffreListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0){
+			String str = ((JButton)arg0.getSource()).getText();
+			Ecran.setText(Ecran.getText()+str);
+		}
+	}
+
+	class EgalListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0){
+		//Ici utiliser la fonction parse_on pour parser la chaine de caracteres récuperée
+		String str = Ecran.getText();
+		EXPR e = PARSER.parse_on(str);
+		int result = e.eval();
+		String resulte = String.valueOf(result);
+		Ecran.setText(resulte);
+		}
+
+	}
+
+	class MoinsListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0){
+			Ecran.setText(Ecran.getText()+"-");
+		}
+	}
+
+	class PlusListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0){
+			Ecran.setText(Ecran.getText()+"+");
+		}
+	}
+
+	class DivListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0){
+			Ecran.setText(Ecran.getText()+"/");
+		}
+	}
+
+	class MulListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0){
+			Ecran.setText(Ecran.getText()+"*");
+		}
+	}
+
+	class ResetListener implements ActionListener {
+		public void actionPerformed(ActionEvent arg0){
+			Ecran.setText("");
+		}
+	}
+
+
+
+	
 
 }
 
